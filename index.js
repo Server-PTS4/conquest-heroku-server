0d03f4014b0716e052c4423332dbdf3b08083ee7
Hackarius <lucas13.paranthoen12@gmail.com>
2017-02-16 12:01:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var app = express();
 const server = require('http').createServer(app);
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const {graphql, GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
 
-var schema = buildSchema(`
-  type Query {
-    hello: String
+const fields = {
+  hello: {
+    type: GraphQLString,
+    args: {
+      name: {type: GraphQLString}
+    },
+    resolve: (_, args) => {
+      return `Hello ${args.name} !`
+    }
   }
-`);
+};
 
-var root = { hello: () => 'Hello world!' };
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQuery',
+    fields
+  })
+});
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+const query = '{ hello (name: "Alan Turing") }';
+
+graphql(schema, query)
+  .then(res => console.log(JSON.stringify(res, null, 2)))
+  .catch(err => console.error(err));
 
 const port = process.env.PORT || 80;
 server.listen(port, () => console.log('Server listening on port 80'));
