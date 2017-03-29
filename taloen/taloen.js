27921bc67102f43e4eba4bcb27f01b24e0afc01d
Unknown <pierrelouistalbot@gmail.com>
2017-03-29 12:00:48 +0200

    Fixed verbose

--- taloen/taloen.js
+++ taloen/taloen.js
@@ -25,7 +25,7 @@ function getData(query) {
 	if (query == "{team{*player}}")
 		list += team.getTeamObject(players);
 	if (query == "{team}") {
-		winston.info('LISTE :' + team.getTeamList().toString());
+		winston.info('LISTE :' + JSON.stringify(team.getTeamList()));
 		return team.getTeamList();
 	}
     if (query == "{spot}")
